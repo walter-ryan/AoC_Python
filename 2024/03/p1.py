c8914c297e237d43bfd53af4a8e920396bfdd137
walter-ryan <wryan2@proton.me>
2024-12-03 12:19:01 +0000

    2024-03 Tidied use of split()

--- 2024/03/p1.py
+++ 2024/03/p1.py
@@ -3,10 +3,9 @@ import re
 with open('2024/03/input.txt') as file:
     line = file.read()
 
-list = [x[4:-1] for x in re.findall(r"mul\(\d+,\d+\)", line)]
+list = [x[4:-1].split(',') for x in re.findall(r"mul\(\d+,\d+\)", line)]
 
 tot = 0
 for pair in list:
-    t = [int(a) for a in pair.split(',')]
-    tot += t[0]*t[1]
+    tot += int(pair[0])*int(pair[1])
 print(tot)
--- 2024/03/p2.py
+++ 2024/03/p2.py
@@ -6,10 +6,9 @@ with open('2024/03/input.txt') as file:
 line = re.sub(r"\s",'',line)
 line = re.sub(r"don't\(\).*?do\(\)|don't\(\).*$",'',line)
 
-list = [x[4:-1] for x in re.findall(r"mul\(\d+,\d+\)", line)]
+list = [x[4:-1].split(',') for x in re.findall(r"mul\(\d+,\d+\)", line)]
 
 tot = 0
 for pair in list:
-    t = [int(a) for a in pair.split(',')]
-    tot += t[0]*t[1]
+    tot += int(pair[0])*int(pair[1])
 print(tot)
