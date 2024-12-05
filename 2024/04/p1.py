3784efac563066ebe70a5ef4b8aadfeb8d510196
walter-ryan <wryan2@proton.me>
2024-12-05 14:10:24 +0000

    2024-04 Parameterised search into function

--- 2024/04/p1.py
+++ 2024/04/p1.py
@@ -1,52 +1,33 @@
-with open('2024/04/input.txt') as file:
+with open('2024/04/test.txt') as file:
     lines = file.readlines()
 
 widthRange = range(len(lines[0].strip()))
 heightRange = range(len(lines))
 xmasCount = 0
+
+def findMAS(xOff: int, yOff: int) -> bool:
+    if i+xOff in heightRange and j+yOff in widthRange and lines[j+yOff][i+xOff] == 'M':
+        if i+2*xOff in heightRange and j+2*yOff in widthRange and lines[j+2*yOff][i+2*xOff] == 'A':
+            if i+3*xOff in heightRange and j+3*yOff in widthRange and lines[j+3*yOff][i+3*xOff] == 'S':
+                return True
+    return False
+directions = [
+    (-1,1),
+    (0,1),
+    (1,1),
+    (-1,0),
+    (1,0),
+    (-1,-1),
+    (0,-1),
+    (1,-1)
+]
 # Find X
 for x in widthRange:
     for y in heightRange:
         if lines[y][x] == 'X':
             i, j = x, y
             # test up
-            if i in heightRange and j - 1 in widthRange and lines[j-1][i] == 'M':
-                if i in heightRange and j - 2 in widthRange and lines[j-2][i] == 'A':
-                    if i in heightRange and j - 3 in widthRange and lines[j-3][i] == 'S':
-                        xmasCount += 1
-            # test down
-            if i in heightRange and j + 1 in widthRange and lines[j+1][i] == 'M':
-                if i in heightRange and j + 2 in widthRange and lines[j+2][i] == 'A':
-                    if i in heightRange and j + 3 in widthRange and lines[j+3][i] == 'S':
-                        xmasCount += 1
-            # test right
-            if i + 1 in heightRange and j in widthRange and lines[j][i+1] == 'M':
-                if i + 2 in heightRange and j in widthRange and lines[j][i+2] == 'A':
-                    if i + 3 in heightRange and j in widthRange and lines[j][i+3] == 'S':
-                        xmasCount += 1
-            # test right
-            if i - 1 in heightRange and j in widthRange and lines[j][i-1] == 'M':
-                if i - 2 in heightRange and j in widthRange and lines[j][i-2] == 'A':
-                    if i - 3 in heightRange and j in widthRange and lines[j][i-3] == 'S':
-                        xmasCount += 1
-            # test upright
-            if i + 1 in heightRange and j - 1 in widthRange and lines[j-1][i+1] == 'M':
-                if i + 2 in heightRange and j - 2 in widthRange and lines[j-2][i+2] == 'A':
-                    if i + 3 in heightRange and j - 3 in widthRange and lines[j-3][i+3] == 'S':
-                        xmasCount += 1
-            # test downright
-            if i + 1 in heightRange and j + 1 in widthRange and lines[j+1][i+1] == 'M':
-                if i + 2 in heightRange and j + 2 in widthRange and lines[j+2][i+2] == 'A':
-                    if i + 3 in heightRange and j + 3 in widthRange and lines[j+3][i+3] == 'S':
-                        xmasCount += 1
-            # test upleft
-            if i - 1 in heightRange and j - 1 in widthRange and lines[j-1][i-1] == 'M':
-                if i - 2 in heightRange and j - 2 in widthRange and lines[j-2][i-2] == 'A':
-                    if i - 3 in heightRange and j - 3 in widthRange and lines[j-3][i-3] == 'S':
-                        xmasCount += 1
-            # test downleft
-            if i - 1 in heightRange and j + 1 in widthRange and lines[j+1][i-1] == 'M':
-                if i - 2 in heightRange and j + 2 in widthRange and lines[j+2][i-2] == 'A':
-                    if i - 3 in heightRange and j + 3 in widthRange and lines[j+3][i-3] == 'S':
-                        xmasCount += 1
+            for (a,b) in directions:
+                if findMAS(a,b):
+                    xmasCount += 1
 print(xmasCount)
