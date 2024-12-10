a344c083e6951ceaf5f825567fad838349c39f44
walter-ryan <wryan2@proton.me>
2024-12-10 08:51:33 +0000

    2024-07

--- 2024/07/p1.py
+++ 2024/07/p1.py
@@ -1,4 +1,29 @@
-with open('2024/07/test.txt') as file:
+from itertools import product
+from tqdm import tqdm
+with open('2024/07/input.txt') as file:
     lines = file.readlines()
 
+l = []
+for line in lines:
+    l.append((int(line.split(':')[0]),[int(x) for x in line.split()[1:]]))
 
+def isSafe(line):
+    for x in product('01', repeat=len(line[1])-1):
+        sum = line[1][0]
+        for i in range(len(line[1])-1):
+            if x[i] == '0':
+                sum += line[1][i+1]
+            elif x[i] == '1':
+                sum *= line[1][i+1]
+            else:
+                sum = int(str(sum) + str(line[1][i+1]))
+            if sum > line[0]:
+                continue
+        if sum == line[0]:
+            return line[0]
+    return 0
+safeSum = 0
+for line in tqdm(l):
+    safeSum += isSafe(line)
+
+print(safeSum)
