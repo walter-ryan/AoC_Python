492884f9f0c00e688ee3129656c88342cc7a09f9
walter-ryan <wryan2@proton.me>
2024-12-10 09:51:29 +0000

    2024-08

--- 2024/08/p2.py
+++ 2024/08/p2.py
@@ -1,4 +1,40 @@
-with open('2024/08/test.txt') as file:
-    lines = file.readlines()
+import string
+from itertools import combinations
+
+with open('2024/08/input.txt') as file:
+    lines = [line.strip() for line in file.readlines()]
 
 
+chars = set(string.ascii_letters) | set('1234567890')
+
+antinodes = set()
+
+for a in chars:
+    letter_positions = []
+    for y in range(len(lines)):
+        for x in range(len(lines[0])):
+            if lines[y][x] == a:
+                letter_positions.append(complex(x, y))
+
+    for (x,y) in combinations(letter_positions,2):
+        i = 0
+        inside = True
+        while inside:
+            an = x + (i*(y - x))
+            if an.real in range(len(lines[0])) and an.imag in range(len(lines)):
+                antinodes.add(an)
+                i += 1
+            else:
+                inside = False
+        i = 0
+        inside = True
+        while inside:
+            an = y + (i*(x - y))
+            if an.real in range(len(lines[0])) and an.imag in range(len(lines)):
+                antinodes.add(an)
+                i += 1
+            else:
+                inside = False
+        
+
+print(len(antinodes))
