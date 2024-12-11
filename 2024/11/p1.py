5bb521280fcba48874966485ae86ef481a47832a
walter-ryan <wryan2@proton.me>
2024-12-11 16:52:14 +0000

    2024-11

--- 2024/11/p1.py
+++ 2024/11/p1.py
@@ -1,4 +1,30 @@
-with open('2024/11/test.txt') as file:
-    lines = file.readlines()
+from functools import cache
+
+with open('2024/11/input.txt') as file:
+    nums = file.read().split()
+
+print(nums)
+
+@cache
+def handleNum(num: str) -> list:
+    if num == '0':
+        return ['1']
+    elif len(num) % 2 == 0:
+        return [str(int(num[:len(num)//2])),str(int(num[len(num)//2:]))]
+    else:
+        return [str(int(num)*2024)]
 
 
+
+def blink(l: list) -> list:
+    nl = []
+    for num in l:
+        nl = nl + handleNum(num)
+    return nl
+
+for i in range(25):
+    nums = blink(nums)
+    print('Step', i)
+    print('Length:',len(nums))
+
+print(len(nums))
--- 2024/11/p2.py
+++ 2024/11/p2.py
@@ -1,4 +1,43 @@
-with open('2024/11/test.txt') as file:
-    lines = file.readlines()
+from functools import cache
+from math import floor, log10
+from tqdm import tqdm
 
+with open('2024/11/input.txt') as file:
+    nums = file.read().split()
+
+
+counts = dict()
+for num in nums:
+    if num not in counts.keys():
+        counts[int(num)] = 1
+    else:
+        counts[int(num)] += 1
+
+
+@cache
+def dupeRocks(num: int) -> list:
+    if num == 0:
+        return [1]
+    l = floor(log10(num))+1
+
+    if l % 2 == 0:
+        return[num // 10**(l//2),num %  10**(l//2)]
+    else:
+        return [num*2024]
+
+
+def blink(counts: dict) -> dict:
+    nd = dict()
+    for n in counts.keys():
+        for m in dupeRocks(n):
+            if m in nd.keys():
+                nd[m] += counts[n]
+            else:
+                nd[m] = counts[n]
+    return nd
+
+
+for i in tqdm(range(75)):
+    counts = blink(counts)
+print('Length:',sum(counts.values()))
 
